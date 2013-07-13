2b7becf5b17cb19af9e97beee5e0208b3a58c0bd
vellvisher <vaarnan@gmail.com>
2013-07-13 03:52:09 -0400

    Redirecting admin to admin/

--- assassins/urls.py
+++ assassins/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import RedirectView
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -12,7 +13,8 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
+    url(r'^admin$', RedirectView.as_view(url='admin/')),
+
     url(r'^admin/', include(admin.site.urls)),
 
     url(r'^handleSms',
